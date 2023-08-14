374f44fde4b25239b06cbcaef24227dbc5934713
Hans Bergren (0xCLARITY) <hansbergren@gmail.com>
2023-08-14 13:29:48 -0700

    Relative paths to use as a Foundry submodule

--- contracts/SplitMain.sol
+++ contracts/SplitMain.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.4;
 
-import {ISplitMain} from 'contracts/interfaces/ISplitMain.sol';
-import {SplitWallet} from 'contracts/SplitWallet.sol';
-import {Clones} from 'contracts/libraries/Clones.sol';
+import {ISplitMain} from './interfaces/ISplitMain.sol';
+import {SplitWallet} from './SplitWallet.sol';
+import {Clones} from './libraries/Clones.sol';
 import {ERC20} from '@rari-capital/solmate/src/tokens/ERC20.sol';
 import {SafeTransferLib} from '@rari-capital/solmate/src/utils/SafeTransferLib.sol';
 
